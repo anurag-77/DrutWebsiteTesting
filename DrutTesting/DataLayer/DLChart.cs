cee019cb940500fa5503de03c29a5cd5bf3ceb15
anurag-77 <anurag.mahadik@arconnet.com>
2021-11-29 14:07:36 +0530

    removed random object from DLChart.cs

--- DrutTesting/DataLayer/DLChart.cs
+++ DrutTesting/DataLayer/DLChart.cs
@@ -20,8 +20,6 @@ namespace DrutTesting.DataLayer
 
             List<int> Yaxis3 = new List<int> { 238, 833, 654 };
 
-            Random random = new Random();
-
             lstYaxisList.Add(Yaxis1);
             lstYaxisList.Add(Yaxis2);
             lstYaxisList.Add(Yaxis3);
