dc284bd8a8c934ca626f9d41ab601271d09d07e6
anurag-77 <anurag.mahadik@arconnet.com>
2021-12-01 11:48:11 +0530

    random removed from DLChart.cs

--- DrutTesting/DataLayer/DLChart.cs
+++ DrutTesting/DataLayer/DLChart.cs
@@ -24,7 +24,6 @@ namespace DrutTesting.DataLayer
             lstYaxisList.Add(Yaxis2);
             lstYaxisList.Add(Yaxis3);
 
-            Random random = new Random();
 
             List<Dictionary<string, string>> data = new List<Dictionary<string, string>>();
             if(!string.IsNullOrEmpty(ChartId))
@@ -35,14 +34,14 @@ namespace DrutTesting.DataLayer
                 keyslist.Add("Top_Available");
                 keyslist.Add("Sales_MID");
 
-                for (int i = 0; i < 30; i++)
+                for (int i = 0; i < 3; i++)
                 {
                     Dictionary<string, string> dictionary = new Dictionary<string, string>();
 
-                    dictionary.Add(keyslist[0], $"Category_{i}");
-                    dictionary.Add(keyslist[1], $"{random.Next(35, 50)}");
-                    dictionary.Add(keyslist[2], $"{random.Next(20, 45)}");
-                    dictionary.Add(keyslist[3], $"{random.Next(30, 40)}");
+                    dictionary.Add(keyslist[0], $"{lstCategoryList[i]}");
+                    dictionary.Add(keyslist[1], $"{lstYaxisList[i][0]}");
+                    dictionary.Add(keyslist[2], $"{lstYaxisList[i][1]}");
+                    dictionary.Add(keyslist[3], $"{lstYaxisList[i][2]}");
 
                     data.Add(dictionary);
                 }
